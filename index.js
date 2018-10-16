9a8bfdc127755cab31eda69b9a74335d54e3dfa3
Wojtek Witkowski <hi@wojtek.im>
2018-10-17 01:37:17 +0200

    add new spam messages to filter

--- index.js
+++ index.js
@@ -4,29 +4,34 @@ const TelegramBot = require('node-telegram-bot-api');
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
+function banAndKick(chatId, userId, msgId) {
+  bot.kickChatMember(chatId, userId);
+  bot.sendMessage(chatId, `👮️ Removed some spam and smashed the banhammer. Move along.`)
+  bot.deleteMessage(chatId, msgId);
+}
+
+bot.onText(/u.to\//, (msg, match) => {
+  const chatId = msg.chat.id;
+
+  console.log(`Spam message: ${msg.text}`)
+  banAndKick(chatId, msg.from.id, msg.message_id)
+})
+
+// Listen for any kind of message
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
-  function banAndKick() {
-    bot.kickChatMember(chatId, msg.from.id);
-    bot.sendMessage(chatId, `👮️ Removed some spam and smashed the banhammer. Move along.`)
-    bot.deleteMessage(chatId, msg.message_id);
-  }
-
-  // send a message to the chat acknowledging receipt of their message
   if (msg.forward_from_chat) {
     console.log(`Forwareded message: ${msg.text}`)
 
     if (msg.entities[0].url) {
       console.log(`Message has a link: ${msg.text}`);
-      banAndKick();
+      banAndKick(chatId, msg.from.id, msg.message_id)
     }
   }
 
   if (msg.text.match(/t.me\/bitcoin/g)) {
     console.log(`Spam message: ${msg.text}`)
-    banAndKick();
+    banAndKick(chatId, msg.from.id, msg.message_id)
   }
 });
